1ad6b5612328011a2a20b16d92a69c796cee391f
Ji Hua <ji.hua@panjiva.com>
2020-07-12 12:59:36 -0400

    finished first html

--- werewolf/login/tests.py
+++ werewolf/login/tests.py
@@ -1,8 +1,18 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from login.views import index
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
+    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        content = response.content.decode('utf-8').strip()
+        self.assertTrue(content.startswith('<html>'))
+        self.assertIn('<title>烂柯游艺社</title>', content)
+        self.assertTrue(content.endswith('</html>'))
--- werewolf/login/views.py
+++ werewolf/login/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world")
+    return HttpResponse("""
+        <html><title>烂柯游艺社</title></html>
+    """)
